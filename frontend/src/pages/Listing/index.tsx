f332afbfea3f0fe394ab88b243c9ccc0c5bf5baf
NataliaRamalho <natalia.ramalho.lopes@gmail.com>
2022-01-14 18:37:45 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,20 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/Movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
-
-  axios.get(`${BASE_URL}/movies?size=12`).then(response => console.log(response.data));
-
+  const [pageNumber, setPagNumber] = useState(0);
+  
+  useEffect(()=>{
+    axios.get(`${BASE_URL}/movies?size=12`).then(response => {
+      const data = response.data as MoviePage;
+      console.log(data)
+      setPagNumber(data.number)
+    });
+  },[])
   return (
     <>
       <Pagination />
